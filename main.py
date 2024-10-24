465ec0729148943e08e29810a55c7a2e32d66166
Geon05 <zqrc05@gmail.com>
2024-10-24 15:26:30 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+st.write(df)
+
+
+st.write('hello world')
+
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+st.dataframe(df)  # 대화형 테이블
+st.table(df)  # 정적 테이블
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.514575, 127.0495556],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+import streamlit as st
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+    
+option = st.selectbox(
+'Which number do you like best?',
+    [1, 2, 3, 4, 5])
+
+'You selected: ', option
+
+st.sidebar.write('연락방법')
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+st.sidebar.write('범위')
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    st.write('해리포터')
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
